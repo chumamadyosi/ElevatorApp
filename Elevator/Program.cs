8e5e138d0120352185e439aa036f6a72492eeeba
ChumaM <ChumaM@ZAEC-BET-1BD314>
2024-11-04 19:52:42 +0200

    Add project files.

---
+++ Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public string Test()
+        {
+            return "";
+        }
+    }
+   
+}
